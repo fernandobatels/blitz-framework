a8e45c63166c3457922fc4f7a91e4a3c45865821
Fernando Batels <luisfbatels@gmail.com>
2016-04-28 13:09:08 -0300

    To github

--- app/routs.php
+++ app/routs.php
@@ -1,15 +1,20 @@
 <?php
-
 /*
- * This file is part of the Blitz package.
- *
- * (c) 2016 Fernando Batels
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * 
- * 
- * 
+ * Blitz Framework - Small Framework to PHP
+ * Copyright (C) 2016 Fernando Batels <luisfbatels@gmail.com>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
