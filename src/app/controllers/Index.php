e97e1b0fb2f1efda060c511ef868d634fe27f99d
Fernando Batels <luisfbatels@gmail.com>
2017-10-16 20:15:49 -0200

    Change licence

--- src/app/controllers/Index.php
+++ src/app/controllers/Index.php
@@ -4,9 +4,7 @@
  * This file is part of the Blitz package.
  *
  * (c) 2016 Fernando Batels
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+
  */
 
 namespace blitz\app\controllers;
--- src/app/models/Cliente.php
+++ src/app/models/Cliente.php
@@ -4,9 +4,6 @@
  * This file is part of the Blitz package.
  *
  * (c) 2016 Fernando Batels
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 namespace blitz\app\models;
 /**
